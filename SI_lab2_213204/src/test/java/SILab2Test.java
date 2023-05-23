731552a92cb3eac0e13a296aa5f621a44bca80fe
Nikola-Jankovikj <108418537+Nikola-Jankovikj@users.noreply.github.com>
2023-05-23 17:48:24 +0200

    Added SI_lab2_213204

---
+++ SI_lab2_213204/src/test/java/SILab2Test.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SILab2Test {
+    @Test
+    void everyBranchForFunction() {
+        assertAll("All branch test:",
+                () -> assertThrows(RuntimeException.class, () -> SILab2.function(null, null)),
+                () -> assertFalse(SILab2.function(new User(null, "pass!", "someone@email.com"),
+                        List.of(new User("someone@email.com", "password!", "someone@email.com"), new User("ricky", "pass!", "someoneElse@email.com")))),
+                () -> assertFalse(SILab2.function(new User("stan", "password 2$", "bademail"), List.of(new User("stan", "password2!", "someone@emai.com")))),
+                () -> assertFalse(SILab2.function(new User("stan", "password2$", "bademail"), List.of(new User("stan", "password2!", "someone@emai.com"))))
+        );
+    }
+
+    @Test
+    void multipleConditionsForFunction(){
+        assertAll("Multiple conditions test:",
+                () -> assertThrows(RuntimeException.class, () -> SILab2.function(null, null)),
+                () -> assertThrows(RuntimeException.class, () -> SILab2.function(new User("username", null, null), null)),
+                () -> assertThrows(RuntimeException.class, () -> SILab2.function(new User("username", "password", null), null)),
+                () -> assertDoesNotThrow(() -> SILab2.function(new User("username", "password", "email"), null))
+        );
+    }
+}
